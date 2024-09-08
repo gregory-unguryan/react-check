6e90355c04c259007f71deddb39591ae8f5426a0
Grigory Unguryan <ungurian.post@yandex.kz>
2024-09-08 08:36:33 +0500

    Version 2

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,12 @@
+import UserProfile from '@/components/user-profile'
+
 function Home() {
 	return (
 		<div className='w-ful h-full items-center p-2'>
-			<span>Logo</span>
+			<header className='flex items-center justify-between p-2'>
+				<span>Logo</span>
+				<UserProfile />
+			</header>
 		</div>
 	)
 }
