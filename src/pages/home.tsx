ac7062b3a62ddc2887cfc7a87c41964ab2992e29
Grigory Unguryan <ungurian.post@yandex.kz>
2024-09-08 14:59:35 +0500

    Version 4 Update

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,16 @@
 import UserProfile from '@/components/user-profile'
+import { Feather } from 'lucide-react'
 
 function Home() {
 	return (
 		<div className='w-ful h-full items-center p-2'>
 			<header className='flex items-center justify-between p-2'>
-				<span>Logo</span>
+				<div className='flex items-center gap-1 group cursor-pointer border px-3 py-1 rounded-full hover:bg-primary transition-all duration-300'>
+					<Feather className='w-4 h-4 group-hover:stroke-muted transition-all duration-300' />
+					<span className='text-muted-foreground group-hover:text-muted transition-all duration-300'>
+						Author
+					</span>
+				</div>
 				<UserProfile />
 			</header>
 		</div>
