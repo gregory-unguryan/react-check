b28a32c3d2bc3f174273db4ce631e19aff95dff3
Grigory Unguryan <ungurian.post@yandex.kz>
2024-09-08 16:38:58 +0500

    Version 5

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -1,13 +1,35 @@
-import { SquareMenu } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import {
+	HoverCard,
+	HoverCardContent,
+	HoverCardTrigger,
+} from '@/components/ui/hover-card'
+import { Contact, ListCheck, Settings, User } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
 export default function RootLayout() {
 	return (
 		<div className='relative h-screen flex flex-col items-center'>
-			<header className='absolute bottom-4 flex items-center justify-between gap-2 px-6 py-4 border rounded-xl text-muted-foreground'>
-				<SquareMenu className='text-accent-foreground' /> Навигация по
-				приложению
-			</header>
+			<HoverCard openDelay={300} closeDelay={100}>
+				<HoverCardTrigger className='absolute -bottom-5'>
+					<Button
+						variant='ringHover'
+						className='w-8 h-8 flex items-center justify-between gap-2 border rounded-full text-muted-foreground bg-muted hover:bg-muted-foreground/10 cursor-pointer transition-all duration-300'
+					/>
+				</HoverCardTrigger>
+				<HoverCardContent
+					className='w-full flex items-center justify-between gap-5 p-4 text-muted'
+					side='top'
+					align='center'
+					sideOffset='2'
+				>
+					<User className='w-5 h-5 text-primary hover:text-primary cursor-pointer' />
+					<ListCheck className='w-5 h-5 hover:text-primary cursor-pointer' />
+					<Settings className='w-5 h-5 hover:text-primary cursor-pointer' />
+					<Contact className='w-5 h-5 hover:text-primary cursor-pointer' />
+				</HoverCardContent>
+			</HoverCard>
+
 			<main className='w-full flex flex-col flex-1'>
 				<Outlet />
 			</main>
