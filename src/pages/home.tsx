914b7c3fc8d331b4bc21d3b0fd828ece662c5660
Gregory Unguryan <ungurian.grigory@yandex.kz>
2024-09-08 17:53:54 +0500

    Version 6 - Refactor padding for header

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import { Feather } from 'lucide-react'
 function Home() {
 	return (
 		<div className='w-ful h-full items-center p-2'>
-			<header className='flex items-center justify-between p-2'>
+			<header className='flex items-center justify-between p-4'>
 				<div className='flex items-center gap-1 group cursor-pointer border px-3 py-1 rounded-full hover:bg-primary transition-all duration-300'>
 					<Feather className='w-4 h-4 group-hover:stroke-muted transition-all duration-300' />
 					<span className='text-muted-foreground group-hover:text-muted transition-all duration-300'>
